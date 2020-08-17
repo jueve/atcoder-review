00f1a6ec0d10b45bb5feddbbc0c8b4987b839bac
jueve <o8g7no9q@gmail.com>
2020-08-17 22:41:44 +0900

    'database-update-log.json'ファイルを生成

---
+++ src/main-process/initialization/createDatabaseUpdateLogFile.ts
@@ -0,0 +1,91 @@
+import * as fs from "fs";
+import { ipcMain } from "electron";
+import { DatabaseUpdateLog } from "../types";
+import { resolvePath } from "../resolvePath";
+import { DATABASE_UPDATE_LOG } from "../constants";
+
+type CreateDatabaseUpdateLogFile =
+  | "CREATE_DATABASE_UPDATE_LOG_FILE"
+  | "CREATE_DATABASE_UPDATE_LOG_FILE_SUCCEEDED"
+  | "CREATE_DATABASE_UPDATE_LOG_FILE_FAILED";
+
+export const CREATE_DATABASE_UPDATE_LOG_FILE =
+  "CREATE_DATABASE_UPDATE_LOG_FILE";
+export const CREATE_DATABASE_UPDATE_LOG_FILE_SUCCEEDED =
+  "CREATE_DATABASE_UPDATE_LOG_FILE_SUCCEEDED";
+export const CREATE_DATABASE_UPDATE_LOG_FILE_FAILED =
+  "CREATE_DATABASE_UPDATE_LOG_FILE_FAILED";
+
+const message: Record<string, string> = {
+  succeeded: "Succeeded to create the database update log file.",
+  failed: "Failed to create the database update log file.",
+  exists: "The database update log file already exists.",
+  broken: "The database update log file was broken, and regenerated new one.",
+};
+
+const init: DatabaseUpdateLog = {
+  contests: null,
+  problems: null,
+  problem_models: null,
+  user_submissions: null,
+};
+
+const propertiesFullFilled = (log: DatabaseUpdateLog): boolean => {
+  return (
+    log.contests !== undefined &&
+    log.problems !== undefined &&
+    log.problem_models !== undefined &&
+    log.user_submissions !== undefined
+  );
+};
+
+export const createDatabaseUpdateLogFile = (
+  begin: CreateDatabaseUpdateLogFile,
+  succeeded: CreateDatabaseUpdateLogFile,
+  failed: CreateDatabaseUpdateLogFile
+): void => {
+  const databaseUpdateLog = resolvePath(DATABASE_UPDATE_LOG);
+  ipcMain.on(begin, async (event) => {
+    try {
+      if (fs.existsSync(databaseUpdateLog)) {
+        const data: string = await fs.promises.readFile(databaseUpdateLog, {
+          encoding: "utf-8",
+        });
+        const schema: DatabaseUpdateLog = JSON.parse(data);
+
+        if (propertiesFullFilled(schema)) {
+          event.reply(succeeded, message.exists);
+        } else {
+          fs.promises
+            .writeFile(databaseUpdateLog, JSON.stringify(init), {
+              encoding: "utf-8",
+              flag: "w",
+            })
+            .then(() => {
+              event.reply(succeeded, message.broken);
+            })
+            .catch((error) => {
+              event.reply(failed, message.failed);
+              console.log(error);
+            });
+        }
+      } else {
+        fs.promises
+          .writeFile(databaseUpdateLog, JSON.stringify(init), {
+            encoding: "utf-8",
+            flag: "w",
+          })
+          .then(() => {
+            event.reply(succeeded, message.succeeded);
+          })
+          .catch((error) => {
+            console.log(error);
+            event.reply(failed, message.failed);
+          });
+      }
+    } catch (error) {
+      console.log(error);
+      event.reply(failed, message.failed);
+    }
+  });
+};

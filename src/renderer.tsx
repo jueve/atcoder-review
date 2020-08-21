9644840c1bf4e43ba61423c67416ab0c4eaeced9
jueve <o8g7no9q@gmail.com>
2020-08-22 01:38:07 +0900

    余分な記述を削除

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -30,12 +30,6 @@ import "./theme/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
-import { ipcRenderer } from "electron";
-import { CREATE_BASE_DIRECTORY } from "./main-process/initialization/createBaseDirectory";
-import { CREATE_CONFIG_FILE } from "./main-process/initialization/createConfigFile";
-import { CREATE_DATABASE_UPDATE_LOG_FILE } from "./main-process/initialization/createDatabaseUpdateLogFile";
-import { CREATE_LOG_FILE } from "./main-process/initialization/createLogFile";
-import { CREATE_DATABASE_TABLES } from "./main-process/initialization/createDatabaseTables";
 import { initialize } from "./initialize";
 
 const root = document.createElement("div");
